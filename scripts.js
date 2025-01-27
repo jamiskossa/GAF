b58e87a0ed64fdc0e4dc10e646e1d3c5ec83b332
jamiskossa <jamiskossa@gmail.com>
2025-01-27 16:01:11 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,19 +1,5 @@
 
 
-// Smooth Scroll for Navigation Links
-document.querySelectorAll('nav a').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href').slice(1);
-        const targetElement = document.getElementById(targetId);
-        if (targetElement) {
-            window.scrollTo({
-                top: targetElement.offsetTop,
-                behavior: 'smooth'
-            });
-        }
-    });
-});
 
 // Form Validation Example
 const contactForm = document.querySelector('.contact-form form');
